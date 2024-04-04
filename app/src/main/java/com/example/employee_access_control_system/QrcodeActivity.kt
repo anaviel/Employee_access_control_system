3461191650342b253359a2809aead147b1d8e511
4216 Kovaleva O. Y <kovaleva.olga.2004@gmail.com>
2024-04-04 18:36:41 +0300

    Добавлена генерация QR-кода

---
+++ app/src/main/java/com/example/employee_access_control_system/QrcodeActivity.kt
@@ -0,0 +1,46 @@
+package com.example.employee_access_control_system
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
+import com.google.zxing.common.BitMatrix
+import android.graphics.Bitmap
+import android.graphics.Color
+
+class QrcodeActivity : AppCompatActivity() {
+    private var im: ImageView? = null
+    private var bGenerate: Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qrcode)
+
+        im = findViewById(R.id.imageView5)
+        bGenerate = findViewById(R.id.button)
+        bGenerate?.setOnClickListener {
+            generateQrCode("ФИО сотрудника")
+        }
+    }
+
+    private fun generateQrCode(text: String) {
+        val multiFormatWriter = MultiFormatWriter()
+        try {
+            val bitMatrix: BitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE, 500, 500)
+            val width: Int = bitMatrix.width
+            val height: Int = bitMatrix.height
+            val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+            for (x in 0 until width) {
+                for (y in 0 until height) {
+                    bitmap.setPixel(x, y, if (bitMatrix.get(x, y)) Color.BLACK else Color.WHITE)
+                }
+            }
+            im?.setImageBitmap(bitmap)
+        } catch (e: WriterException) {
+            e.printStackTrace()
+        }
+    }
+}
